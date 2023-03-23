3a09ae560457027f63ea855ca6daf80b80066ffc
Samir <samir.hadrovic@hotmail.com>
2023-03-23 22:06:06 +0100

    dodati propertiji, sredjeni dependenciji

--- src/test/java/rs/booka/BaseTest.java
+++ src/test/java/rs/booka/BaseTest.java
@@ -1,22 +1,30 @@
 package rs.booka;
 
 import java.time.Duration;
+import java.util.ResourceBundle;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BaseTest {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
+    
+	protected static ResourceBundle generalSettings = ResourceBundle.getBundle("general-settings");
+	
+	@BeforeClass
+	static void initTests() {
+        //dodato zbog greske sa Chrome driverom
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        
+	}
 
     @Test
     public void setUp(){
-        //dodato zbog greske sa Chrome driverom
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
-        //WebDriverManager.chromedriver().setup();
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
